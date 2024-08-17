06b3f8a14f75a98723ba50358d3b7e3d4b4d4354
AisAif <Isbahrulfahni@gmail.com>
2024-08-18 00:41:52 +0700

    chore: setup database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
