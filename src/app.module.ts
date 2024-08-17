660b38cb3dadaa12ceb187b384835dac0ea99b7a
AisAif <Isbahrulfahni@gmail.com>
2024-08-17 23:56:06 +0700

    setup nestjs graphql

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.graphql',
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
