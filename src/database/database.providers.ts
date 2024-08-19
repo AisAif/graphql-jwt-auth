5e57aa3ff2b02256f47ab908aca91f374b07e618
AisAif <Isbahrulfahni@gmail.com>
2024-08-19 16:17:07 +0700

    chore: update db configuration according to environment

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'graphql_jwt_auth',
+        host: process.env.DB_HOST || 'localhost',
+        port: +process.env.DB_PORT || 3306,
+        username: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || 'root',
+        database: process.env.DB_NAME || 'graphql_jwt_auth',
         entities: [__dirname + '/../**/*.model{.ts,.js}'],
         synchronize: true,
       });
