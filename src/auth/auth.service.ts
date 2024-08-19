dae0832a8b59287d14dea4720c27a2e7dc026064
AisAif <Isbahrulfahni@gmail.com>
2024-08-19 12:03:17 +0700

    chore: setup service; add register, validate user and login service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { LoginInput } from './dto/login.input';
+import { RegisterInput } from './dto/register.input';
+import * as bcrypt from 'bcryptjs';
+import { User } from 'src/user/user.model';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(registerInput: RegisterInput) {
+    if (registerInput.password !== registerInput.password_confirmation) {
+      throw new BadRequestException('Passwords do not match');
+    }
+
+    if (await this.usersService.findOne(registerInput.username)) {
+      throw new BadRequestException('Username already exists');
+    }
+
+    return this.usersService.save({
+      name: registerInput.name,
+      username: registerInput.username,
+      password: bcrypt.hashSync(registerInput.password, 10),
+    });
+  }
+
+  async validateUser(username: string, pass: string): Promise<User> {
+    const user = await this.usersService.findOne(username);
+    if (user && bcrypt.compareSync(pass, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    return null;
+  }
+
+  async login(loginInput: LoginInput) {
+    const user = await this.usersService.findOne(loginInput.username);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.id, username: user.username, name: user.name };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      user,
+    };
+  }
+}
