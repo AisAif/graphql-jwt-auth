dfd2cce7fa0c85669842469274f54d3dcc569ceb
AisAif <Isbahrulfahni@gmail.com>
2024-08-19 13:00:27 +0700

    chore: register all auth providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/user/user.module';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthResolver } from './auth.resolver';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        global: true,
+        signOptions: { expiresIn: configService.get<number>('JWT_EXPIRATION') },
+      }),
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, AuthResolver],
+  exports: [AuthService],
+})
 export class AuthModule {}
