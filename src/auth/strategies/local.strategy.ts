93dba39c0668a8fb1b5116258a1f64eda63a0e29
AisAif <Isbahrulfahni@gmail.com>
2024-08-19 12:07:20 +0700

    chore: add local strategy for login

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from '../auth.service';
+import { User } from 'src/user/user.model';
+import { Strategy } from 'passport-local';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async validate(username: string, password: string): Promise<User> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+  
+    return user;
+  }
+}
+
